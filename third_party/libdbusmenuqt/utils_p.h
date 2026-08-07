06bb672781c6cf5b0cb84db9c9a03f5fbeba1359
Sniffer <funnyhacker902@gmail.com>
2026-08-07 16:18:57 +0400

    vendor: add KDE dbusmenu mnemonic helper

---
+++ third_party/libdbusmenuqt/utils_p.h
@@ -0,0 +1,13 @@
+/* This file is part of the dbusmenu-qt library
+    SPDX-FileCopyrightText: 2010 Canonical
+    SPDX-FileContributor: Aurelien Gateau <aurelien.gateau@canonical.com>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+
+#include <QChar>
+
+class QString;
+
+QString swapMnemonicChar(const QString &in, QChar src, QChar dst);
