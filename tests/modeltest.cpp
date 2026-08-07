2d3bb7c08041c693c6417693f2e83d9299cbc4be
Sniffer <funnyhacker902@gmail.com>
2026-08-07 11:31:56 +0400

    test: cover incremental dbusmenu property updates

--- tests/modeltest.cpp
+++ tests/modeltest.cpp
@@ -167,7 +167,8 @@ void GlobalMenuModelTest::importsLayoutAndForwardsEvents()
     QVERIFY(fileAction);
     QVERIFY(fileAction->menu());
     QCOMPARE(fileAction->menu()->actions().size(), 2);
-    QCOMPARE(fileAction->menu()->actions().at(0)->text(), QStringLiteral("&Open"));
+    QAction *openAction = fileAction->menu()->actions().at(0);
+    QCOMPARE(openAction->text(), QStringLiteral("&Open"));
     QVERIFY(!fileAction->menu()->actions().at(1)->isEnabled());
 
     QAction *aboutAction = model.data(model.index(1, 0), GlobalMenuModel::ActionRole).value<QAction *>();
@@ -176,7 +177,7 @@ void GlobalMenuModelTest::importsLayoutAndForwardsEvents()
 
     model.aboutToShow(0);
     model.menuOpened(0);
-    fileAction->menu()->actions().at(0)->trigger();
+    openAction->trigger();
     model.menuClosed(0);
     aboutAction->trigger();
 
@@ -185,6 +186,76 @@ void GlobalMenuModelTest::importsLayoutAndForwardsEvents()
     QTRY_VERIFY_WITH_TIMEOUT(fakeMenu.hasEvent(10, QStringLiteral("clicked")), 5000);
     QTRY_VERIFY_WITH_TIMEOUT(fakeMenu.hasEvent(1, QStringLiteral("closed")), 5000);
     QTRY_VERIFY_WITH_TIMEOUT(fakeMenu.hasEvent(2, QStringLiteral("clicked")), 5000);
+
+    const auto sendPropertiesUpdated = [&connection](const DBusMenuItemList &updated, const DBusMenuItemKeysList &removed) {
+        QDBusMessage signal = QDBusMessage::createSignal(
+            QString::fromLatin1(menuPath),
+            QString::fromLatin1(menuInterface),
+            QStringLiteral("ItemsPropertiesUpdated"));
+        signal << QVariant::fromValue(updated) << QVariant::fromValue(removed);
+        QVERIFY(connection.send(signal));
+    };
+
+    const int layoutCallsBeforeProperties = fakeMenu.getLayoutCalls;
+
+    DBusMenuItem openUpdate;
+    openUpdate.id = 10;
+    openUpdate.properties.insert(QStringLiteral("label"), QStringLiteral("_Open Recent"));
+    openUpdate.properties.insert(QStringLiteral("enabled"), false);
+
+    DBusMenuItem aboutUpdate;
+    aboutUpdate.id = 2;
+    aboutUpdate.properties.insert(QStringLiteral("label"), QStringLiteral("_Info"));
+
+    sendPropertiesUpdated({openUpdate, aboutUpdate}, {});
+    QTRY_COMPARE_WITH_TIMEOUT(openAction->text(), QStringLiteral("&Open Recent"), 5000);
+    QTRY_VERIFY_WITH_TIMEOUT(!openAction->isEnabled(), 5000);
+    QTRY_COMPARE_WITH_TIMEOUT(
+        model.data(model.index(1, 0), GlobalMenuModel::LabelRole).toString(),
+        QStringLiteral("Info"),
+        5000);
+    QTest::qWait(100);
+    QCOMPARE(fakeMenu.getLayoutCalls, layoutCallsBeforeProperties);
+
+    DBusMenuItemKeys openRemoved;
+    openRemoved.id = 10;
+    openRemoved.properties.append(QStringLiteral("enabled"));
+    sendPropertiesUpdated({}, {openRemoved});
+    QTRY_VERIFY_WITH_TIMEOUT(openAction->isEnabled(), 5000);
+    QTest::qWait(100);
+    QCOMPARE(fakeMenu.getLayoutCalls, layoutCallsBeforeProperties);
+
+    DBusMenuItem hideFile;
+    hideFile.id = 1;
+    hideFile.properties.insert(QStringLiteral("visible"), false);
+    DBusMenuItem hideAbout;
+    hideAbout.id = 2;
+    hideAbout.properties.insert(QStringLiteral("visible"), false);
+    sendPropertiesUpdated({hideFile, hideAbout}, {});
+    QTRY_VERIFY_WITH_TIMEOUT(!model.menuAvailable(), 5000);
+    QVERIFY(!fileAction->isVisible());
+    QVERIFY(!aboutAction->isVisible());
+    QTest::qWait(100);
+    QCOMPARE(fakeMenu.getLayoutCalls, layoutCallsBeforeProperties);
+
+    DBusMenuItemKeys showFile;
+    showFile.id = 1;
+    showFile.properties.append(QStringLiteral("visible"));
+    DBusMenuItemKeys showAbout;
+    showAbout.id = 2;
+    showAbout.properties.append(QStringLiteral("visible"));
+    sendPropertiesUpdated({}, {showFile, showAbout});
+    QTRY_VERIFY_WITH_TIMEOUT(model.menuAvailable(), 5000);
+    QVERIFY(fileAction->isVisible());
+    QVERIFY(aboutAction->isVisible());
+    QTest::qWait(100);
+    QCOMPARE(fakeMenu.getLayoutCalls, layoutCallsBeforeProperties);
+
+    DBusMenuItem structuralUpdate;
+    structuralUpdate.id = 2;
+    structuralUpdate.properties.insert(QStringLiteral("children-display"), QStringLiteral("submenu"));
+    sendPropertiesUpdated({structuralUpdate}, {});
+    QTRY_VERIFY_WITH_TIMEOUT(fakeMenu.getLayoutCalls > layoutCallsBeforeProperties, 5000);
 
     connection.unregisterObject(QString::fromLatin1(menuPath), QDBusConnection::UnregisterNode);
 }
